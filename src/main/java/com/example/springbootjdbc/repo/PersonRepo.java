9de10254e54e5eed56dd5bff7df96a54b48fe26c
RoderCode <Rodersnow@gmail.com>
2023-01-23 17:05:53 +0100

    add selectPersonByPassword

--- src/main/java/com/example/springbootjdbc/repo/PersonRepo.java
+++ src/main/java/com/example/springbootjdbc/repo/PersonRepo.java
@@ -9,23 +9,29 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class PersonRepo {
 
     private final JdbcTemplate jdbcTemplate;
+
     @Autowired
     public PersonRepo(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Person selectPersonByUsername(String username){
+    public Person selectPersonByUsername(String username) {
         try {
-            return jdbcTemplate.queryForObject("SELECT username FROM login WHERE username=?",
-                    new BeanPropertyRowMapper<>(Person.class),username);
-        }catch (EmptyResultDataAccessException e) {
+            return jdbcTemplate.queryForObject("SELECT username FROM coworker WHERE username=?",
+                    new BeanPropertyRowMapper<>(Person.class), username);
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
+    }
+
+    public Person selectPersonByPassword(String password){
+        try {
+            return jdbcTemplate.queryForObject("SELECT password FROM coworker WHERE password=?",
+                    new BeanPropertyRowMapper<>(Person.class),password);
+        }catch (EmptyResultDataAccessException e){
+            return null;
         }
-        
-
-
-
+    }
 
 
 }
