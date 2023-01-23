03074869aa6ab599f5e224348c5813a986feeef2
RoderCode <Rodersnow@gmail.com>
2023-01-23 17:08:47 +0100

    add @Repo annotions to personRepo

--- src/main/java/com/example/springbootjdbc/repo/PersonRepo.java
+++ src/main/java/com/example/springbootjdbc/repo/PersonRepo.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PersonRepo {
 
     private final JdbcTemplate jdbcTemplate;
