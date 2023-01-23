0b092953ad6fd83492708b5fbdb9f099c260c810
RoderCode <Rodersnow@gmail.com>
2023-01-23 18:02:40 +0100

    send user to invoicePage if correct username or password. if not the user will be send to the home page

--- src/main/java/com/example/springbootjdbc/Controllers/PersonController.java
+++ src/main/java/com/example/springbootjdbc/Controllers/PersonController.java
@@ -1,23 +1,30 @@
 package com.example.springbootjdbc.Controllers;
 
+import com.example.springbootjdbc.service.AuthService;
 import com.example.springbootjdbc.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PersonController {
 
-    private final PersonService personService;
+    private final AuthService authService;
+
     @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
+    public PersonController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("home")
-    public String login(@RequestParam String username, @RequestParam String password){
-        return "redirect:invoice";
+    public String login(@RequestParam String username, @RequestParam String password, ModelMap model) {
+        if (authService.checkIfValidLogin(username,password))
+            return "redirect:invoice";
+        else{
+            model.put("error","incorrect Username or Password");
+            return "home";
+        }
     }
-
 }
