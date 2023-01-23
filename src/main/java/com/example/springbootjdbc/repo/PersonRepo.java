9e855112d3232a0a3e522d115217ee8f5ca8a69d
RoderCode <Rodersnow@gmail.com>
2023-01-23 17:01:48 +0100

    add selectPersonByUsername

--- src/main/java/com/example/springbootjdbc/repo/PersonRepo.java
+++ src/main/java/com/example/springbootjdbc/repo/PersonRepo.java
@@ -1,6 +1,9 @@
 package com.example.springbootjdbc.repo;
 
+import com.example.springbootjdbc.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class PersonRepo {
@@ -11,5 +14,18 @@ public class PersonRepo {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    
+    public Person selectPersonByUsername(String username){
+        try {
+            return jdbcTemplate.queryForObject("SELECT username FROM login WHERE username=?",
+                    new BeanPropertyRowMapper<>(Person.class),username);
+        }catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+        }
+        
+
+
+
+
+
 }
