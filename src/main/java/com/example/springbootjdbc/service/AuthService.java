ce23ae866b799f4e22b331739f5b9c6d06e6f58e
RoderCode <Rodersnow@gmail.com>
2023-01-23 17:58:42 +0100

    create a checkIfVValidLogin method

--- src/main/java/com/example/springbootjdbc/service/AuthService.java
+++ src/main/java/com/example/springbootjdbc/service/AuthService.java
@@ -12,5 +12,11 @@ public class AuthService {
         this.personService = personService;
     }
 
-    
+    public boolean checkIfValidLogin(String username, String password){
+        boolean isUsernameValid = personService.selectUserByUsername(username).equals(username);
+        boolean isPasswordValid = personService.selectUserByPassword(password).equals(password);
+        return isUsernameValid && isPasswordValid;
+    }
+
+
 }
