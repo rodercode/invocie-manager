2f39670d5d718bf5ada622dcec43dd7a538f270f
RoderCode <Rodersnow@gmail.com>
2023-01-23 17:36:14 +0100

    declare and assigne personRepo in PersonService

--- src/main/java/com/example/springbootjdbc/service/PersonService.java
+++ src/main/java/com/example/springbootjdbc/service/PersonService.java
@@ -1,4 +1,17 @@
 package com.example.springbootjdbc.service;
 
+import com.example.springbootjdbc.repo.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+
+    private final PersonRepo personRepo;
+    @Autowired
+    public PersonService(PersonRepo personRepo) {
+        this.personRepo = personRepo;
+    }
+
+    
 }
