be68caabc333ba9e60b5ddfe76d92dd782fd7f2e
RoderCode <Rodersnow@gmail.com>
2023-01-23 17:46:02 +0100

    add content to Person Pojo

--- src/main/java/com/example/springbootjdbc/model/Person.java
+++ src/main/java/com/example/springbootjdbc/model/Person.java
@@ -1,4 +1,37 @@
 package com.example.springbootjdbc.model;
-
 public class Person {
+    private String username;
+    private String password;
+
+    public Person() {
+    }
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
