a95bdbdb3d475deb8ec8cdfae0c31c6eda350acc
RoderCode <Rodersnow@gmail.com>
2023-01-24 11:15:49 +0100

    connect InvoiceListService with InvoiceListRepo

--- src/main/java/com/example/springbootjdbc/service/InvoiceListService.java
+++ src/main/java/com/example/springbootjdbc/service/InvoiceListService.java
@@ -1,6 +1,15 @@
 package com.example.springbootjdbc.service;
 
+import com.example.springbootjdbc.repo.InvoiceListRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InvoiceListService {
 
-
+    private final InvoiceListRepo invoiceListRepo;
+    @Autowired
+    public InvoiceListService(InvoiceListRepo invoiceListRepo) {
+        this.invoiceListRepo = invoiceListRepo;
+    }
 }
