95262a7ffa2aef8cc74fa08542af3041c1944acc
RoderCode <Rodersnow@gmail.com>
2023-01-24 11:47:27 +0100

    add connection from invoiceService to InvoiceController

--- src/main/java/com/example/springbootjdbc/Controllers/InvoiceController.java
+++ src/main/java/com/example/springbootjdbc/Controllers/InvoiceController.java
@@ -3,6 +3,7 @@ package com.example.springbootjdbc.Controllers;
 import com.example.springbootjdbc.model.Invoice;
 import com.example.springbootjdbc.model.Person;
 import com.example.springbootjdbc.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,11 @@ import java.text.SimpleDateFormat;
 
 @Controller
 public class InvoiceController {
-    
+    private final InvoiceService invoiceService;
+    @Autowired
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
 
     @PostMapping("payment")
     public String createInvoice(@RequestParam String title, @RequestParam String description,
