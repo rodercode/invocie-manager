4a68d6be97eb2d804cd1d781d2e735459015da69
RoderCode <Rodersnow@gmail.com>
2023-01-26 13:47:56 +0100

    reduce code in createInvoice method

--- src/main/java/com/example/springbootjdbc/Controllers/InvoiceController.java
+++ src/main/java/com/example/springbootjdbc/Controllers/InvoiceController.java
@@ -8,10 +8,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class InvoiceController {
@@ -28,10 +25,9 @@ public class InvoiceController {
         return "invoice";
     }
     @PostMapping("payment")
-    public String createInvoice(HttpSession session,@RequestParam String title, @RequestParam String description,
-                                @RequestParam String category, @RequestParam int price) {
-
-        invoiceService.addInvoice(new Invoice((String) session.getAttribute("username"),title, description, category,price));
+    public String createInvoice(HttpSession session, @ModelAttribute Invoice invoice) {
+        invoice.setId_coworker((String) session.getAttribute("username"));
+        invoiceService.addInvoice(invoice);
         return "payment";
     }
     @PostMapping("edit*")
