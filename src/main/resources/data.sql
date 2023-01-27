ac8e56b78d1fe246beb9c26dbe7d21bc51529034
RoderCode <Rodersnow@gmail.com>
2023-01-27 16:36:16 +0100

    add values and table for testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,38 @@
 DROP TABLE IF EXISTS coworker;
+DROP TABLE IF EXISTS invoice;
 
 CREATE TABLE coworker (
 username VARCHAR(55) PRIMARY KEY,
 password VARCHAR(55)
 );
 
+CREATE TABLE invoice(
+id INT AUTO_INCREMENT PRIMARY KEY,
+id_coworker VARCHAR(55),
+title VARCHAR(255),
+description VARCHAR(255),
+category VARCHAR(255),
+price INT,
+created_at date
+);
+
 INSERT INTO coworker(username, password)
 VALUES
-    ("leif","stockholmärmitthjärta"),
-    ("lisa", "glenfrångöteborg"),
-    ("johan", "kaffeärbäst"),
-    ("marcus", "norrlandärguld");
+("leif","stockholmärmitthjärta"),
+("lisa", "glenfrångöteborg"),
+("johan", "kaffeärbäst"),
+("marcus", "norrlandärguld");
+
+
+INSERT INTO invoice(id_coworker,title, description, category, price,created_at)
+VALUES
+("marcus","mat", "LunchMöte", "Möte",60,now()),
+("lisa","övertid", "jobbat över 3 timmar", "övertid",300,now()),
+("leif","resa", "från göteborg till luleå", "resersättning",100,now()),
+("johan","sjuk", "skada i armen", "sjukersättning",500,now());
+
+
+
 
 
 
